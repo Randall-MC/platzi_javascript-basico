65bd8887cba94734e60dec4098ba86ab711e5633
Randall-ATG15 <randallmarcu@gmial.com>
2023-02-01 15:18:02 -0600

    rock, paper, scissors gane finished

--- challenges/gameRPS.js
+++ challenges/gameRPS.js
@@ -1,35 +1,33 @@
+/* Challenge - Rock, Paper, Scissors
+- Generate the variables rock, paper and scissors
+- Generate a function that receive a parameter that is the user option will play with.
+- Inside of the function, create a validation to know if you win or lose
+- Return the result of the match */
+
 // Players
-let playerOne;
 let ai;
-// Options
+let player;
+// Options (variables)
 let rock = "rock";
 let paper = "paper";
 let scissors = "scissors";
 let options = ["rock", "paper", "scissors"];
-// Events
-let winner = "You win";
-let loser = "You lose";
-let draw = "Draw";
-let result;
 
 game(rock);
 
-
-// Rules
-function game(playerChoice) {
-    playerOne = playerChoice;
-    ai = Math.floor(Math.random()*3);
-
-    if (playerOne === rock && ai === scissors) {
-        result = winner;
-    } else if (playerOne === paper && ai === rock) {
-        result = winner;
-    } else if (playerOne === scissors && ai === paper) {
-        result = winner;
-    } else if (playerOne === ai) {
-        result = draw;
+// Validation
+function game(choice) {
+    player = choice;
+    ai = options[Math.floor(Math.random()*3)];
+    if (player === rock && ai === scissors) {
+        return console.log(`You win - ${player} vs ${ai}`);
+    } else if (player === paper && ai === rock) {
+        return console.log(`You win - ${player} vs ${ai}`);
+    } else if (player === scissors && ai === paper) {
+        return console.log(`You win - ${player} vs ${ai}`);
+    } else if (player === ai) {
+        return console.log(`Draw - ${player} vs ${ai}`);
     } else {
-        result = loser;
+        return console.log(`You lose - ${player} vs ${ai}`);
     }
-    return console.log(result, playerOne, ai);
 }
